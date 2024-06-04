cd4ce21bd7629ef8394f0cb3dd452d977e5dbfb2
Marty808s <134133128+Marty808s@users.noreply.github.com>
2024-06-04 17:44:20 +0200

    update stylování, heatmap na věkové skupiny

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ import dash_bootstrap_components as dbc
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 fig_scatter_country_count = px.scatter(title="Přehled uživatel jednotlivch zemí")
-fig_gender_country = px.scatter(title="Heatmap - pohlaví jednotlivých obyvatel")
+fig_vek_skupin = px.scatter(title="Heatmap - pohlaví jednotlivých obyvatel")
 fig_bar_sex = px.bar(title="Přehled pohlaví - berem v potaz pouze 2!")
 fig_map_scatter = px.scatter_geo(title="Mapa - země a počet uživatelů")
 fig_registered_time = px.line(title="Čas registrace")
 
 app.layout = html.Div([
-    html.H1("Přehled uživatelů aplikace", style={'textAlign': 'center', "margin": "10px"}),
+    html.H1("Přehled uživatelů aplikace", style={'textAlign': 'center', "margin": "10px", "padding": "20px"}),
     dbc.Card(
        [
            dbc.CardBody(
@@ -25,35 +25,50 @@ app.layout = html.Div([
                ]
            )
        ],
-       style={"width": "18rem", "margin": "10px", "backgroundColor": "#222", "borderColor": "#333"}
+       style={"width": "18rem", "margin": "10px", "padding-bottom": "10px", "backgroundColor": "#222", "borderColor": "#333"}
    ),
+
+
     dcc.Dropdown(
         id='country-filter', 
         options=[],
         multi=True, 
-        placeholder="Vyberte země",
+        placeholder="Vyberte země jako globální filtr pro celý dashboard",
         style={"margin": "10px auto", "width": "80%", "maxWidth": "80%", "backgroundColor": "#222", "borderColor": "#333", "color": "black"}),
-    dcc.Graph(id='scatter-country-count', figure=fig_scatter_country_count),
-    dcc.Graph(id='gender-country-graph', figure=fig_gender_country),
-    dcc.Graph(id='sex-bargraph', figure=fig_bar_sex),
-    dcc.Graph(id='map-scatter', figure=fig_map_scatter),  # Opraveno z fix_map_scatter na fig_map_scatter
-    dcc.Graph(id='registered-time', figure=fig_registered_time),
-    dcc.Interval(
-            id='interval-component',
-            interval=2*1000,  # Interval pro update DB
-            n_intervals=0
-        ),
-    html.Div([
+
+        dcc.Graph(id='scatter-country-count', figure=fig_scatter_country_count),
+
+        dcc.Graph(id='vek-skupin', figure=fig_vek_skupin),
+
+        dcc.Graph(id='sex-bargraph', figure=fig_bar_sex),
+
+        dcc.Graph(id='map-scatter', figure=fig_map_scatter),
+
         dcc.DatePickerRange(
             id='date-picker-range',
             start_date_placeholder_text="Start Date",
             end_date_placeholder_text="End Date",
-            display_format='YYYY-MM-DD'
+            display_format='YYYY-MM-DD',
+            style={"margin": "10px auto", "width": "80%", "padding": "10px", "maxWidth": "80%", "backgroundColor": "#222", "color": "black"}),
+
+        dcc.Graph(id='registered-time', figure=fig_registered_time),
+
+        dcc.Interval(
+                id='interval-component',
+                interval=30*1000,  # Interval pro update DB
+                n_intervals=0
+            ),
+
+    html.Footer(
+        html.P(
+            "Made with ❤ by Martin Vlnas!",
+            style={"textAlign": "center", "margin": "10px 0"}
         )
-    ], style={"margin": "10px auto", "width": "80%", "padding": "10px", "maxWidth": "80%", "backgroundColor": "#222", "borderColor": "#333", "color": "black"})
+    )
+    
 ])
 @app.callback(
-    [Output('gender-country-graph', 'figure'),
+    [Output('vek-skupin', 'figure'),
      Output('sex-bargraph', 'figure'),
      Output('scatter-country-count', 'figure'),
      Output('map-scatter', 'figure'),
@@ -92,9 +107,16 @@ def update_graphs(n, country_filter, start_date, end_date):
     scatter_country_count = df.groupby(['country']).size().reset_index(name='count')
     fig_scatter_country_count = px.scatter(scatter_country_count, x="country", y="count", size="count", color="count", title="Přehled uživatel jednotlivch zemí")
 
-    # Heatmap - gender
-    gender_country_count = df.groupby(['country', 'gender']).size().unstack(fill_value=0)
-    fig_gender_country = px.imshow(gender_country_count, height=600, aspect='auto', title="Heatmap - pohlaví jednotlivch obyvatel")
+    # Heatmap - vekove skupiny
+    df['dob'] = pd.to_datetime(df['dob'])
+    df['age'] = (pd.Timestamp('now') - df['dob']).dt.days // 365
+    bins = [0, 18, 30, 45, 60, 75, 100]
+    labels = ['0-18', '19-30', '31-45', '46-60', '61-75', '76-100']
+    df['age_group'] = pd.cut(df['age'], bins=bins, labels=labels, right=False)
+
+    age_gender_country_count = df.groupby(['country', 'age_group']).size().unstack(fill_value=0)
+    fig_age_gender_country = px.imshow(age_gender_country_count, height=600, aspect='auto', title="Heatmap - věkové skupiny")
+
     
     # Bar plot - gender by country
     gender_country_data = df.groupby(['country', 'gender']).size().reset_index(name='count')
@@ -111,11 +133,9 @@ def update_graphs(n, country_filter, start_date, end_date):
         except:
             return None
 
-
     country_counts['country_code'] = country_counts['country'].apply(get_country_code)
     country_counts = country_counts[country_counts['country_code'].notna()]
     country_filter = [{'label': country, 'value': code} for country, code in zip(country_counts['country'], country_counts['country_code'])]
-    country_codes = [item['value'] for item in country_filter]
     print(country_filter)
     fig_map_scatter = px.scatter_geo(country_counts, locations="country_code", size="count", color="count", height=800, title="Mapa - země a počet uživatelů")
     
@@ -124,7 +144,7 @@ def update_graphs(n, country_filter, start_date, end_date):
     fig_registered_time = px.line(registered_time_country, x="registered", y="count", color="country", title="Čas registrace podle země", range_x=[start_date, end_date])
     
     # Return grafů
-    return fig_gender_country, fig_bar_sex, fig_scatter_country_count, fig_map_scatter, country_options, f'Total Users: {total_users}',fig_registered_time
+    return fig_age_gender_country, fig_bar_sex, fig_scatter_country_count, fig_map_scatter, country_options, f'Total Users: {total_users}',fig_registered_time
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8050, dev_tools_hot_reload=True)
--- data.py
+++ data.py
@@ -73,7 +73,7 @@ def add_user():
         cnx.close()
 
 def schedule_user_addition():
-    threading.Timer(1, schedule_user_addition).start()
+    threading.Timer(10, schedule_user_addition).start()
     add_user()
     print("User added")
     get_data()
